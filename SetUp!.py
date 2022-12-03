7f3fbfeb33095fbec092ffd66912f039133222be
Thoisoi Three <you@example.com>
2022-12-03 17:11:06 +0300

    master

--- SetUp!.py
+++ SetUp!.py
@@ -6,7 +6,7 @@ def main():
     #trash setup
     trash = c4d.BaseObject(c4d.Onull)
     doc.InsertObject(trash)
-    trash.SetName('Trash / Archive')
+    trash.SetName('Trash / Archive') #folder name
     trash[c4d.ID_BASELIST_ICON_COLORIZE_MODE] = 2
     trash[c4d.ID_BASELIST_ICON_COLOR] = c4d.Vector(0.18824, 0.70196, 0.74902)
     trash[c4d.ID_BASELIST_ICON_FILE] = str(440000193)
@@ -16,7 +16,7 @@ def main():
     #geometry setup
     geometry = c4d.BaseObject(c4d.Onull)
     doc.InsertObject(geometry)
-    geometry.SetName('Geometry')
+    geometry.SetName('Geometry') #folder name
     geometry[c4d.ID_BASELIST_ICON_COLORIZE_MODE] = 2
     geometry[c4d.ID_BASELIST_ICON_COLOR] = c4d.Vector(0, 0.66667, 1)
     geometry[c4d.ID_BASELIST_ICON_FILE] = str(13364)
@@ -25,7 +25,7 @@ def main():
     #lights setup
     light = c4d.BaseObject(c4d.Onull)
     doc.InsertObject(light)
-    light.SetName('Light Setup')
+    light.SetName('Light Setup') #folder name
     light[c4d.ID_BASELIST_ICON_COLORIZE_MODE] = 2
     light[c4d.ID_BASELIST_ICON_COLOR] = c4d.Vector(1, 0.83529, 0)
     light[c4d.ID_BASELIST_ICON_FILE] = str(200000031)
@@ -34,22 +34,21 @@ def main():
     #cams setup
     cam = c4d.BaseObject(c4d.Onull)
     doc.InsertObject(cam)
-    cam.SetName('Cameras')
+    cam.SetName('Cameras') #folder name
     cam[c4d.ID_BASELIST_ICON_COLORIZE_MODE] = 2
     cam[c4d.ID_BASELIST_ICON_COLOR] = c4d.Vector(1, 0.21961, 0.14902)
     cam[c4d.ID_BASELIST_ICON_FILE] = str(18170)
     cam[c4d.NULLOBJECT_DISPLAY] = 14
     
     c4d.EventAdd()
+
     text = """
     SetUp is ready!
-    
+
     If the scene is empty just add an object!
     """
 
-
     c4d.gui.MessageDialog(text)
-
 
 if __name__ == '__main__':
     main()
