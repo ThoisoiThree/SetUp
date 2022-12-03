403132d3abcfe3ca4dd3e7c60134a3a662e2e882
Thoisoi Three <you@example.com>
2022-12-03 17:25:58 +0300

    master

--- SetUp!.py
+++ SetUp!.py
@@ -11,9 +11,9 @@ def main():
     doc.InsertObject(trash)
     trash.SetName('Trash / Archive') #folder name
     trash[c4d.ID_BASELIST_ICON_COLORIZE_MODE] = 2
-    trash[c4d.ID_BASELIST_ICON_COLOR] = c4d.Vector(0.18824, 0.70196, 0.74902)
+    trash[c4d.ID_BASELIST_ICON_COLOR] = c4d.Vector(0.18824, 0.70196, 0.74902) #icon color
     trash[c4d.ID_BASELIST_ICON_FILE] = str(440000193)
-    trash[c4d.NULLOBJECT_DISPLAY] = 4
+    trash[c4d.NULLOBJECT_DISPLAY] = 4 #change to 14 for invisible point
     trash[c4d.NULLOBJECT_ORIENTATION] = 3
 
     #geometry setup
@@ -21,7 +21,7 @@ def main():
     doc.InsertObject(geometry)
     geometry.SetName('Geometry') #folder name
     geometry[c4d.ID_BASELIST_ICON_COLORIZE_MODE] = 2
-    geometry[c4d.ID_BASELIST_ICON_COLOR] = c4d.Vector(0, 0.66667, 1)
+    geometry[c4d.ID_BASELIST_ICON_COLOR] = c4d.Vector(0, 0.66667, 1) #icon color
     geometry[c4d.ID_BASELIST_ICON_FILE] = str(13364)
     geometry[c4d.NULLOBJECT_DISPLAY] = 14
 
@@ -30,7 +30,7 @@ def main():
     doc.InsertObject(light)
     light.SetName('Light Setup') #folder name
     light[c4d.ID_BASELIST_ICON_COLORIZE_MODE] = 2
-    light[c4d.ID_BASELIST_ICON_COLOR] = c4d.Vector(1, 0.83529, 0)
+    light[c4d.ID_BASELIST_ICON_COLOR] = c4d.Vector(1, 0.83529, 0) #icon color
     light[c4d.ID_BASELIST_ICON_FILE] = str(200000031)
     light[c4d.NULLOBJECT_DISPLAY] = 14
 
@@ -39,7 +39,7 @@ def main():
     doc.InsertObject(cam)
     cam.SetName('Cameras') #folder name
     cam[c4d.ID_BASELIST_ICON_COLORIZE_MODE] = 2
-    cam[c4d.ID_BASELIST_ICON_COLOR] = c4d.Vector(1, 0.21961, 0.14902)
+    cam[c4d.ID_BASELIST_ICON_COLOR] = c4d.Vector(1, 0.21961, 0.14902) #icon color
     cam[c4d.ID_BASELIST_ICON_FILE] = str(18170)
     cam[c4d.NULLOBJECT_DISPLAY] = 14
     
