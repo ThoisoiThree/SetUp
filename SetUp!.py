de7bf0fda820adfc5b3e315c5142df9505f95ab6
Thoisoi <91261768+ThoisoiThree@users.noreply.github.com>
2022-12-04 14:06:43 +0300

    Update SetUp!.py

--- SetUp!.py
+++ SetUp!.py
@@ -13,7 +13,7 @@ def main():
     trash[c4d.ID_BASELIST_ICON_COLORIZE_MODE] = 2
     trash[c4d.ID_BASELIST_ICON_COLOR] = c4d.Vector(0.18824, 0.70196, 0.74902) #icon color
     trash[c4d.ID_BASELIST_ICON_FILE] = str(440000193)
-    trash[c4d.NULLOBJECT_DISPLAY] = 4 #change to 14 for invisible point
+    trash[c4d.NULLOBJECT_DISPLAY] = 14
     trash[c4d.NULLOBJECT_ORIENTATION] = 3
 
     #geometry setup
