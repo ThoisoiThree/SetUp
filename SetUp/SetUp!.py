68d831ddadce195d5c208400dc760975266a133c
Thoisoi Three <you@example.com>
2022-12-30 15:47:06 +0300

    dev

--- SetUp/SetUp!.py
+++ SetUp/SetUp!.py
@@ -1,45 +1,5 @@
 import c4d
 
-def folderSetup():
-
-    #*trash setup
-    trash = c4d.BaseObject(c4d.Onull)
-    doc.InsertObject(trash)
-    trash.SetName('Trash / Archive') #folder name
-    trash[c4d.ID_BASELIST_ICON_COLORIZE_MODE] = 2
-    trash[c4d.ID_BASELIST_ICON_COLOR] = c4d.Vector(0.70196, 0.70196, 0.70196) #icon color
-    trash[c4d.ID_BASELIST_ICON_FILE] = str(440000193)
-    trash[c4d.NULLOBJECT_DISPLAY] = 14
-
-    #*geometry setup
-    geometry = c4d.BaseObject(c4d.Onull)
-    doc.InsertObject(geometry)
-    geometry.SetName('Geometry') #folder name
-    geometry[c4d.ID_BASELIST_ICON_COLORIZE_MODE] = 2
-    geometry[c4d.ID_BASELIST_ICON_COLOR] = c4d.Vector(0, 0.66667, 1) #icon color
-    geometry[c4d.ID_BASELIST_ICON_FILE] = str(13364)
-    geometry[c4d.NULLOBJECT_DISPLAY] = 14
-
-    #*lights setup
-    light = c4d.BaseObject(c4d.Onull)
-    doc.InsertObject(light)
-    light.SetName('Light Setup') #folder name
-    light[c4d.ID_BASELIST_ICON_COLORIZE_MODE] = 2
-    light[c4d.ID_BASELIST_ICON_COLOR] = c4d.Vector(1, 0.83529, 0) #icon color
-    light[c4d.ID_BASELIST_ICON_FILE] = str(200000031)
-    light[c4d.NULLOBJECT_DISPLAY] = 14
-
-    #*cams setup
-    cam = c4d.BaseObject(c4d.Onull)
-    doc.InsertObject(cam)
-    cam.SetName('Cameras') #folder name
-    cam[c4d.ID_BASELIST_ICON_COLORIZE_MODE] = 2
-    cam[c4d.ID_BASELIST_ICON_COLOR] = c4d.Vector(1, 0.21961, 0.14902) #icon color
-    cam[c4d.ID_BASELIST_ICON_FILE] = str(18170)
-    cam[c4d.NULLOBJECT_DISPLAY] = 14
-    
-    c4d.EventAdd()
-
 def main():
 
     #* A dictionary mapping object types to folder names
